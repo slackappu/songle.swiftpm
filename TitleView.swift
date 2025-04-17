cc21d57c45f18df4d6397646fee89f2da35e2f8d
David <dniedojadlo7291@stu.d214.org>
2025-04-17 12:42:05 -0500

    add to the text Songle making it ore appealing. aswell as adding to the button that takes it to the GuessView.

--- TitleView.swift
+++ TitleView.swift
@@ -3,11 +3,20 @@ import SwiftUI
 struct TitleView: View {
     var body: some View {
         VStack{
-            Text(" 🎵 Songle 🎵")
-                .font(.largeTitle)
-                .font(.custom("Times New Roman", size: 40))
-                .bold()
-            Divider()
+            Text("Songle")
+                .font(.system(size: 50, weight: .bold))
+                   .foregroundStyle(
+                       LinearGradient(
+                        gradient: Gradient(colors: [.purple, .blue, .green]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing
+                       )
+                   )
+                   .background(.black)
+                       .foregroundColor(.white)
+                       .cornerRadius(10)
+                       .shadow(color: .pink, radius: 10, x: 0, y: 0)
+                       .shadow(color: .purple, radius: 20, x: 0, y: 0)
         }
         VStack(spacing: 10){
             Text("How to Play")
@@ -25,6 +34,10 @@ struct TitleView: View {
             
         NavigationStack{
             NavigationLink("Go to Guess View", destination: GuessView())
+                .font(.largeTitle)
+                .foregroundStyle(.blue)
+                .background(.black)
+                .cornerRadius(15)
             }
         }
     }
