dfc7f54cbf560e1b8c540f3322451466aee19949
Leo Jovanov <ljovanov7002@stu.d214.org>
2025-04-17 12:42:54 -0500

    Created textfield for user to enter their song guess.

--- GuessView.swift
+++ GuessView.swift
@@ -1,14 +1,37 @@
 import SwiftUI
 
 struct GuessView: View {
+    @State var userGuess: String = ""
     var body: some View {
         VStack {
-            Image("Bristol")
-                .resizable()
-                .frame(width: 300, height: 300)
-                .blur(radius: 20)
-            Text("Song: Bristol")
-            Text("Artist: Feng")
+            Text("Guess the Song 🎶")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+            
+            TextField("Enter your song guess", text: $userGuess)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 300)
+                .padding(.bottom, 20)
+            
+            Button {
+                print("User's guess: \(userGuess)")
+            } label: {
+                Text("Submit Guess")
+                    .font(.title3)
+                    .padding()
+                    .background(Color.red)
+                    .foregroundStyle(.white)
+                    .cornerRadius(10)
+            }
+
+//            Image("Bristol")
+//                .resizable()
+//                .frame(width: 300, height: 300)
+//                .blur(radius: 20)
+//            Text("Song: Bristol")
+//            Text("Artist: Feng")
         }
     }
 }
