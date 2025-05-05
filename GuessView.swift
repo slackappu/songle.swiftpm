47a1d80b16e703affdbe53f0d791182667af4941
Aryan Prajapati <aprajapati7330@stu.d214.org>
2025-05-05 09:02:07 -0700

    guesses cant go below 0, new varialbe and added to button

--- GuessView.swift
+++ GuessView.swift
@@ -16,6 +16,7 @@ struct GuessView: View {
     @State var revealSong = false
     @State var guessCount = 0
     @State var showHintButton = false
+    @State var maxGuesses = false
     var body: some View {
         VStack{
             Text("Guess the Song 🎶")
@@ -29,7 +30,7 @@ struct GuessView: View {
                     .foregroundStyle(.gray)
             }
             VStack(spacing:15){
-                Text("Guesses left: \(6 - guessCount)")
+                Text("Guesses left: \(max(0, 6 - guessCount))")
                 //            Text("Song: Girls Trip")
                 //            Text("Artist: YT")
                 //            Image("oi")
@@ -42,7 +43,7 @@ struct GuessView: View {
             }
             .font(.custom("Futura", size: 18))
         }
-        .padding(3)
+    
         HStack(spacing: 30){
             Button {
                 audioManager.playPause()
@@ -79,153 +80,155 @@ struct GuessView: View {
         Text("Make sure you have proper spelling!")
             .font(.custom("Futura", size: 16))
             .foregroundStyle(.gray)
-            .padding(.bottom, 20)
+            .padding(.bottom, 10)
         Button("Submit Guess"){
-            checkTheGuess()
-        }
-        .font(.custom("Futura", size: 24))
-        .padding()
-        .background(Color.red)
-        .foregroundStyle(.white)
-        .cornerRadius(10)
-        .disabled(isCorrect)
-        .alert(isPresented: $showAlert){
-            Alert(
-                title: Text(isCorrect ? "Correct" : "Incorrect."),
-                message: Text(isCorrect ? "Congratulations!" : "Try Again. You can do this!"),
-                dismissButton: .default(Text("OK"))
-            )
-        }
-        if guessCount >= 2 {
-            Button(action: {
-                Alerthi = true
-            }) {
-                Text("Tap For Hints")
-                    .frame(width:150, height: 40)
-                    .background(.orange)
-                    .foregroundStyle(.white)
-                    .font(.custom("Futura", size: 23))
-                    .cornerRadius(10)
-                    .shadow(color: .blue, radius: 5)
+            if guessCount >= 6 {
+                maxGuesses = true
+            } else {
+                checkTheGuess()
             }
-            .alert(isPresented: $Alerthi) {
-                Alert(
-                    title: Text("🎶 Fun Fact!"),
-                    message: Text("""
+        }
+                .font(.custom("Futura", size: 24))
+                .padding()
+                .background(Color.red)
+                .foregroundStyle(.white)
+                .cornerRadius(10)
+                .disabled(isCorrect)
+                .alert(isPresented: $showAlert){
+                    Alert(
+                        title: Text(isCorrect ? "Correct" : "Incorrect."),
+                        message: Text(isCorrect ? "Congratulations!" : "Try Again. You can do this!"),
+                        dismissButton: .default(Text("OK"))
+                    )
+                }
+            if guessCount >= 2 {
+                Button(action: {
+                    Alerthi = true
+                }) {
+                    Text("Tap For Hints")
+                        .frame(width:150, height: 40)
+                        .background(.orange)
+                        .foregroundStyle(.white)
+                        .font(.custom("Futura", size: 23))
+                        .cornerRadius(10)
+                        .shadow(color: .blue, radius: 5)
+                }
+                .alert(isPresented: $Alerthi) {
+                    Alert(
+                        title: Text("🎶 Fun Fact!"),
+                        message: Text("""
                               Made By Playboi Carti.
                               Carti's first number-one song on the Billboard Hot 100.
                               """),
-                    dismissButton: .default(Text("Nice!"))
-                )
+                        dismissButton: .default(Text("Nice!"))
+                    )
+                }
+            }
+            if guessCount >= 6 {
+                Button(action: {
+                    revealSong = true
+                }) {
+                    Text("Reveal Song!")
+                        .font(.title3)
+                        .foregroundStyle(.white)
+                        .padding()
+                        .background(.green)
+                        .cornerRadius(10)
+                        .shadow(color: .blue, radius: 5)
+                }
+                .alert(isPresented: $revealSong) {
+                    Alert(title: Text("Song Details"), message: Text("• Song: Long Time \n • Artist: Playboi Carti \n • Year Released: 2018"), dismissButton: .default(Text("Nice Try!")))
+                }
             }
         }
-        if guessCount >= 6 {
-            Button(action: {
-                revealSong = true
-            }) {
-                Text("Reveal Song!")
-                    .font(.title3)
-                    .foregroundStyle(.white)
-                    .padding()
-                    .background(.green)
-                    .cornerRadius(10)
-                    .shadow(color: .blue, radius: 5)
-            }
-            .alert(isPresented: $revealSong) {
-                Alert(title: Text("Song Details"), message: Text("• Song: Long Time \n • Artist: Playboi Carti \n • Year Released: 2018"), dismissButton: .default(Text("Nice Try!")))
+        func checkTheGuess(){
+            let trimmedGuess = userGuess.trimmingCharacters(in: .whitespacesAndNewlines)
+            userPreviousGuesses.append(trimmedGuess)
+            
+            let answer = "long time"
+            isCorrect = trimmedGuess.lowercased() == answer
+            showAlert = true
+            userGuess = ""
+            
+            guessCount += 1
+            if guessCount >= 2 {
+                showHintButton = true
             }
         }
+        //    func stopSong(){
+        //        audioPlayer?.stop()
+        //        audioManager.isPlaying = false
+        //        audioPlayer?.currentTime = 0
+        //    }
+        //    func startSong(){
+        //        let soundName = "longTime"
+        //        guard let soundFile = NSDataAsset(name: soundName) else {
+        //            print("👺 \(soundName) is an invalid sound file")
+        //            return
+        //        }
+        //        do {
+        //            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+        //            duration = audioPlayer?.duration ?? 0
+        //            audioPlayer?.play()
+        //            audioManager.isPlaying = true
+        //            autoStop()
+        //            startTimer()
+        //        } catch {
+        //            print("Error: \(error.localizedDescription) from creating audio player")
+        //        }
+        //    }
+        
+        //    func autoStop() {
+        //        timer?.invalidate()
+        //        timer = Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { _ in
+        //            DispatchQueue.main.async{
+        //                if let player = audioPlayer, player.isPlaying {
+        //                    stopSong()
+        //                }
+        //            }
+        //        }
+        //    }
+        //        func startTimer() {
+        //            var newTimer: Timer?
+        //                newTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+        //                    DispatchQueue.main.async {
+        //                        if let player = audioPlayer {
+        //                            currentTime = player.currentTime
+        //                            if !player.isPlaying {
+        //                                timer?.invalidate()
+        //                            }
+        //                        }
+        //                    }
+        //                }
+        //        timer = newTimer
+        //            }
+        //
+        //            func playSong(){
+        //                if let player = audioPlayer {
+        //                    if player.isPlaying {
+        //                        player.pause()
+        //                        audioManager.isPlaying = false
+        //       //                 timer?.invalidate()
+        //                    } else {
+        //                        player.play()
+        //                        audioManager.isPlaying = true
+        //                        autoStop()
+        //                    }
+        //                } else {
+        //                    startSong()
+        //                    audioManager.isPlaying = true
+        //                    //            autoStop()
+        //                }
+        //            }
+    }
+    func formatTime(time: TimeInterval) -> String {
+        let minutes = Int(time) / 60
+        let seconds = Int(time) % 60
+        return String(format: "%d:%02d", minutes, seconds)
     }
     
-    
-    func checkTheGuess(){
-        let trimmedGuess = userGuess.trimmingCharacters(in: .whitespacesAndNewlines)
-        userPreviousGuesses.append(trimmedGuess)
-        
-        let answer = "long time"
-        isCorrect = trimmedGuess.lowercased() == answer
-        showAlert = true
-        userGuess = ""
-        
-        guessCount += 1
-        if guessCount >= 2 {
-            showHintButton = true
-        }
+
+    #Preview {
+        GuessView()
     }
-    //    func stopSong(){
-    //        audioPlayer?.stop()
-    //        audioManager.isPlaying = false
-    //        audioPlayer?.currentTime = 0
-    //    }
-    //    func startSong(){
-    //        let soundName = "longTime"
-    //        guard let soundFile = NSDataAsset(name: soundName) else {
-    //            print("👺 \(soundName) is an invalid sound file")
-    //            return
-    //        }
-    //        do {
-    //            audioPlayer = try AVAudioPlayer(data: soundFile.data)
-    //            duration = audioPlayer?.duration ?? 0
-    //            audioPlayer?.play()
-    //            audioManager.isPlaying = true
-    //            autoStop()
-    //            startTimer()
-    //        } catch {
-    //            print("Error: \(error.localizedDescription) from creating audio player")
-    //        }
-    //    }
-    
-    //    func autoStop() {
-    //        timer?.invalidate()
-    //        timer = Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { _ in
-    //            DispatchQueue.main.async{
-    //                if let player = audioPlayer, player.isPlaying {
-    //                    stopSong()
-    //                }
-    //            }
-    //        }
-    //    }
-    //        func startTimer() {
-    //            var newTimer: Timer?
-    //                newTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
-    //                    DispatchQueue.main.async {
-    //                        if let player = audioPlayer {
-    //                            currentTime = player.currentTime
-    //                            if !player.isPlaying {
-    //                                timer?.invalidate()
-    //                            }
-    //                        }
-    //                    }
-    //                }
-    //        timer = newTimer
-    //            }
-    //
-    //            func playSong(){
-    //                if let player = audioPlayer {
-    //                    if player.isPlaying {
-    //                        player.pause()
-    //                        audioManager.isPlaying = false
-    //       //                 timer?.invalidate()
-    //                    } else {
-    //                        player.play()
-    //                        audioManager.isPlaying = true
-    //                        autoStop()
-    //                    }
-    //                } else {
-    //                    startSong()
-    //                    audioManager.isPlaying = true
-    //                    //            autoStop()
-    //                }
-    //            }
-}
-func formatTime(time: TimeInterval) -> String {
-    let minutes = Int(time) / 60
-    let seconds = Int(time) % 60
-    return String(format: "%d:%02d", minutes, seconds)
-}
-
-
-#Preview {
-    GuessView()
-}
 
