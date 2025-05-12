0892030c348fd8f14b57c02293ebb9e0c583f6c7
slackappu <alexveliyathumalil21@gmail.com>
2025-05-12 11:34:04 -0700

    fixed the navigationstack

--- GuessView.swift
+++ GuessView.swift
@@ -16,146 +16,147 @@ struct GuessView: View {
     @State var navigateBack = false
     
     var body: some View {
-        NavigationView {
-        ZStack{
-            audioManager.backgroundColor
-                .ignoresSafeArea()
-                .animation(.easeInOut, value: audioManager.backgroundColor)
-            VStack {
-                VStack{
-                    Image("songle")
-                        .resizable()
-                        .frame(width: 200, height: 200)
-                    Text("Guess the Song 🎶")
-                        .font(.custom("Futura", size: 30))
-                        .fontWeight(.bold)
+        NavigationStack {
+            ZStack{
+                audioManager.backgroundColor
+                    .ignoresSafeArea()
+                    .animation(.easeInOut, value: audioManager.backgroundColor)
+                VStack {
+                    VStack{
+                        Image("songle")
+                            .resizable()
+                            .frame(width: 200, height: 200)
+                        Text("Guess the Song 🎶")
+                            .font(.custom("Futura", size: 30))
+                            .fontWeight(.bold)
+                        
+                        ForEach(userPreviousGuesses, id: \.self) { guess in
+                            Text("• \(guess)")
+                                .font(.custom("Futura", size: 20))
+                                .foregroundStyle(.gray)
+                        }
+                        VStack(spacing:15){
+                            Text("Guesses left: \(max(0, 6 - guessCount))")
+                            //            Text("Song: Girls Trip")
+                            //            Text("Artist: YT")
+                            //            Image("oi")
+                            //                .resizable()
+                            //                .frame(width: 300, height: 300)
+                            //                .blur(radius: 20)
+                            //            Text("Song: Bristol")
+                            //            Text("Artist: Feng")
+                            Text("Time: \(formatTime(time: audioManager.currentTime)) / \(formatTime(time: audioManager.duration))")
+                        }
+                        .font(.custom("Futura", size: 18))
+                        .padding()
+                    }
                     
-                    ForEach(userPreviousGuesses, id: \.self) { guess in
-                        Text("• \(guess)")
-                            .font(.custom("Futura", size: 20))
-                            .foregroundStyle(.gray)
+                    HStack(spacing: 30){
+                        Button {
+                            audioManager.playPause()
+                            print("User's guess: \(userGuess)")
+                        } label: {
+                            Image(systemName: audioManager.isPlaying ? "pause.circle.fill" : "play.circle.fill")
+                                .resizable()
+                                .frame(width: 35, height: 35)
+                        }
+                        Button(action: {
+                            audioManager.restart()
+                        }){
+                            Image(systemName: "arrow.clockwise.circle.fill")
+                                .resizable()
+                                .foregroundColor(.blue)
+                                .frame(width: 35, height: 35)
+                                .opacity(audioManager.isPlaying ? 1.0 : 0.4)
+                        }
+                        .disabled(!audioManager.isPlaying)
                     }
-                    VStack(spacing:15){
-                        Text("Guesses left: \(max(0, 6 - guessCount))")
-                        //            Text("Song: Girls Trip")
-                        //            Text("Artist: YT")
-                        //            Image("oi")
-                        //                .resizable()
-                        //                .frame(width: 300, height: 300)
-                        //                .blur(radius: 20)
-                        //            Text("Song: Bristol")
-                        //            Text("Artist: Feng")
-                        Text("Time: \(formatTime(time: audioManager.currentTime)) / \(formatTime(time: audioManager.duration))")
+                    
+                    TextField("Enter your song guess", text: $userGuess)
+                        .font(.custom("Futura", size: 18))
+                        .multilineTextAlignment(.center)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(width: 300)
+                        .disabled(isCorrect)
+                    Text("Make sure you have proper spelling!")
+                        .font(.custom("Futura", size: 16))
+                        .foregroundStyle(.gray)
+                        .padding(.bottom, 10)
+                    
+                    Button("Submit Guess"){
+                        if guessCount >= 6 {
+                            maxGuesses = true
+                        } else {
+                            checkTheGuess()
+                        }
                     }
-                    .font(.custom("Futura", size: 18))
+                    .font(.custom("Futura", size: 22))
                     .padding()
-                }
-                
-                HStack(spacing: 30){
-                    Button {
-                        audioManager.playPause()
-                        print("User's guess: \(userGuess)")
-                    } label: {
-                        Image(systemName: audioManager.isPlaying ? "pause.circle.fill" : "play.circle.fill")
-                            .resizable()
-                            .frame(width: 35, height: 35)
-                    }
-                    Button(action: {
-                        audioManager.restart()
-                    }){
-                        Image(systemName: "arrow.clockwise.circle.fill")
-                            .resizable()
-                            .foregroundColor(.blue)
-                            .frame(width: 35, height: 35)
-                            .opacity(audioManager.isPlaying ? 1.0 : 0.4)
-                    }
-                    .disabled(!audioManager.isPlaying)
-                }
-                
-                TextField("Enter your song guess", text: $userGuess)
-                    .font(.custom("Futura", size: 18))
-                    .multilineTextAlignment(.center)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .frame(width: 300)
+                    .background(Color.red)
+                    .foregroundStyle(.white)
+                    .cornerRadius(10)
                     .disabled(isCorrect)
-                Text("Make sure you have proper spelling!")
-                    .font(.custom("Futura", size: 16))
-                    .foregroundStyle(.gray)
-                    .padding(.bottom, 10)
-                
-                Button("Submit Guess"){
-                    if guessCount >= 6 {
-                        maxGuesses = true
-                    } else {
-                        checkTheGuess()
-                    }
-                }
-                .font(.custom("Futura", size: 22))
-                .padding()
-                .background(Color.red)
-                .foregroundStyle(.white)
-                .cornerRadius(10)
-                .disabled(isCorrect)
-                .alert(isPresented: $showAlert){
-                    Alert(
-                        title: Text(isCorrect ? "Correct" : "Incorrect."),
-                        message: Text(isCorrect ? "Congratulations!" : "Try Again. You can do this!"),
-                        dismissButton: .default(Text("OK"))
-                    )
-                }
-                if guessCount >= 2 {
-                    Button(action: {
-                        Alerthi = true
-                    }) {
-                        Text("Tap For Hints")
-                            .frame(width:150, height: 40)
-                            .background(.orange)
-                            .foregroundStyle(.white)
-                            .font(.custom("Futura", size: 23))
-                            .cornerRadius(10)
-                            .shadow(color: .blue, radius: 5)
-                    }
-                    .alert(isPresented: $Alerthi) {
+                    .alert(isPresented: $showAlert){
                         Alert(
-                            title: Text("🎶 Fun Fact!"),
-                            message: Text("""
+                            title: Text(isCorrect ? "Correct" : "Incorrect."),
+                            message: Text(isCorrect ? "Congratulations!" : "Try Again. You can do this!"),
+                            dismissButton: .default(Text("OK"))
+                        )
+                    }
+                    if guessCount >= 2 {
+                        Button(action: {
+                            Alerthi = true
+                        }) {
+                            Text("Tap For Hints")
+                                .frame(width:150, height: 40)
+                                .background(.orange)
+                                .foregroundStyle(.white)
+                                .font(.custom("Futura", size: 23))
+                                .cornerRadius(10)
+                                .shadow(color: .blue, radius: 5)
+                        }
+                        .alert(isPresented: $Alerthi) {
+                            Alert(
+                                title: Text("🎶 Fun Fact!"),
+                                message: Text("""
                               Made By Playboi Carti.
                               Carti's first number-one song on the Billboard Hot 100.
                               """),
-                            dismissButton: .default(Text("Nice!"))
-                        )
-                    }
-                }
-                if guessCount >= 6 {
-                    Button(action: {
-                        revealSong = true
-                    }) {
-                        Text("Reveal Song!")
-                            .font(.custom("Futura", size: 17))
-                            .padding()
-                            .background(.green)
-                            .cornerRadius(10)
-                            .shadow(color: .blue, radius: 5)
-                    }
-                    .alert(isPresented: $revealSong) {
-                        Alert(title: Text("Song Details"), message: Text("• Song: Long Time \n • Artist: Playboi Carti \n • Year Released: 2018"), dismissButton: .default(Text("Nice Try!")))
-                    }
-                }
-                
-                if isCorrect {
-                    NavigationLink(destination: TitleView(), isActive: $navigateBack) {
-                        Button("Go Back to Title View") {
-                            navigateBack = true
+                                dismissButton: .default(Text("Nice!"))
+                            )
                         }
-                        .font(.custom("Futura", size: 22))
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundStyle(.white)
-                        .cornerRadius(10)
+                    }
+                    if guessCount >= 6 {
+                        Button(action: {
+                            revealSong = true
+                        }) {
+                            Text("Reveal Song!")
+                                .font(.custom("Futura", size: 17))
+                                .padding()
+                                .background(.green)
+                                .cornerRadius(10)
+                                .shadow(color: .blue, radius: 5)
+                        }
+                        .alert(isPresented: $revealSong) {
+                            Alert(title: Text("Song Details"), message: Text("• Song: Long Time \n • Artist: Playboi Carti \n • Year Released: 2018"), dismissButton: .default(Text("Nice Try!")))
+                        }
+                    }
+                    
+                    if isCorrect {
+                        NavigationLink(destination: TitleView(), isActive: $navigateBack) {
+                            Button("Go Back to Title View") {
+                                navigateBack = true
+                            }
+                            .font(.custom("Futura", size: 22))
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundStyle(.white)
+                            .cornerRadius(10)
+                        }
                     }
                 }
+                .animation(.easeInOut, value: audioManager.backgroundColor)
             }
-            .animation(.easeInOut, value: audioManager.backgroundColor)
         }
     }
     
